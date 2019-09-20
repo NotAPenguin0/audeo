9e7cb709061a186bad3d2ce619576616efd445c5
NotAPenguinmvg <33437@vitaetpax.be>
2019-09-20 21:31:50 +0200

    fucking with cmake

--- include/audeo/export_import.hpp
+++ include/audeo/export_import.hpp
@@ -1,15 +1,20 @@
 #ifndef AUDEO_EXPORT_IMPORT_HPP_
-#define AUDEO_EXPORT_IMPORT_HPP_
+#    define AUDEO_EXPORT_IMPORT_HPP_
 
-#ifdef _WIN32
-#    ifdef AUDEO_EXPORT
-#        define AUDEO_API __declspec(dllexport)
+#    ifdef _WIN32
+#        ifdef AUDEO_EXPORT
+#            define AUDEO_API __declspec(dllexport)
+#        else
+#            define AUDEO_API __declspec(dllimport)
+#        endif // AUDEO_EXPORT
 #    else
-#        define AUDEO_API __declspec(dllimport)
-#    endif // AUDEO_EXPORT
-#else
+#        define AUDEO_API
+
+#    endif // _WIN32
+
+// Build static lib. No need for and dll stuff
+#    undef AUDEO_API
 #    define AUDEO_API
 
-#endif // _WIN32
-
 #endif // AUDEO_EXPORT_IMPORT_HPP_
+
